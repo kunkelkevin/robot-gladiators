9e9fc959e72978087e28ffa98ee9b9118fbb0f62
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-03-31 10:01:28 -0500

    quicker interface for shop option

--- assets/js/game.js
+++ assets/js/game.js
@@ -155,21 +155,26 @@ var endGame = function () {
 var shop = function () {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
+    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for "REFILL", 2 for "UPGRADE", or  3 for "LEAVE" to make a choice.'
   );
 
   // use switch case to carry out action
+  debugger;
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   switch (shopOptionPrompt) {
     case "REFILL":
     case "refill":
+    case 1:  
       // increase health and decrease money
       playerInfo.refillHealth();
       break;
     case "UPGRADE":
     case "upgrade":
+    case 2:  
       // increase attack and decrease money
       playerInfo.upgradeAttack();
       break;
+    case 3:  
     case "LEAVE":
     case "leave":
       window.alert("Leaving the store.");
